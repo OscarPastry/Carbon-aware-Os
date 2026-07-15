5bd15840590c8fd9973bc0a00a2f25f8026ef9cf
Rajarshi Pattanayak <pattanayakrajarshi@gmail.com>
2026-07-15 23:02:47 +0530

    Delete os_rust/target directory

--- os_rust/target/debug/deps/socket2-2150c62c61699cba.d
+++
@@ -1,9 +0,0 @@
-/home/Rishi/Documents/coding_Projects/OS_project/os_rust/target/debug/deps/socket2-2150c62c61699cba.d: /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/lib.rs /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sockaddr.rs /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/socket.rs /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sockref.rs /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sys/unix.rs
-
-/home/Rishi/Documents/coding_Projects/OS_project/os_rust/target/debug/deps/libsocket2-2150c62c61699cba.rmeta: /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/lib.rs /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sockaddr.rs /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/socket.rs /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sockref.rs /home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sys/unix.rs
-
-/home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/lib.rs:
-/home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sockaddr.rs:
-/home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/socket.rs:
-/home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sockref.rs:
-/home/Rishi/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/socket2-0.6.3/src/sys/unix.rs:
--- os_rust/target/release/os_project.d
+++
@@ -1,1 +0,0 @@
-/home/Rishi/Documents/coding_Projects/OS_project/os_rust/target/release/os_project: /home/Rishi/Documents/coding_Projects/OS_project/os_rust/src/main.rs
